4af615c82d2b58a9c29b880d0322c81e48260352
João Paulo <joaopdejesus79@gmail.com>
2021-09-25 18:12:23 -0300

    Programa Finalizado!

--- Program.cs
+++ Program.cs
@@ -8,14 +8,9 @@ namespace IdadeAluno
         {
             DateTime data1 = new DateTime(2003, 10, 29), data2 = new DateTime(2021, 10, 29);
             string nascimento = data1.ToLongDateString(), aniversario = data2.ToLongDateString();
-            DateTime semana1 = Convert.ToDateTime(nascimento), semana2 = Convert.ToDateTime(aniversario);
             
-            
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            
-            Console.WriteLine(                
-            $"\nEu nasci em {nascimento}\nFaço 18 anos em {aniversario}.\n");
-
+            Console.ForegroundColor = ConsoleColor.DarkYellow;           
+            Console.WriteLine($"\nEu nasci em {nascimento}\nFaço 18 anos em {aniversario}.\n");
             Console.ResetColor();
         }
     }
